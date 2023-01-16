0faacbf34bc8535aed6f2f1fac161ff8a99d3f8d
Shimon Asulin <asulin.shimon699@gmail.com>
2023-01-16 11:40:16 +0200

    "

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -23,7 +23,7 @@ exports.zoomCheck = (req, res) => {
   // Webhook request event type is a challenge-response check
   if (req.body.event === "endpoint.url_validation") {
     const hashForValidate = crypto
-      .createHmac("sha256", ZOOM_WEBHOOK_SECRET_TOKEN)
+      .createHmac("sha256", nFmrNDXkSKmr87NRy1j8hg)
       .update(req.body.payload.plainToken)
       .digest("hex");
 
