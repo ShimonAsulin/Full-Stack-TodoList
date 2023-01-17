06edc7d878bcb1977d72755c2e2566daaf0cb7eb
Shimon Asulin <asulin.shimon699@gmail.com>
2023-01-17 16:26:06 +0200

    test

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,6 +1,29 @@
 const crypto = require("crypto");
 const Todo = require("../models/todo");
 const token = process.env.ZOOM_WEBHOOK_SECRET_TOKEN;
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+
+// zoom
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+
+
+client.initialize();
+
+
+// ////
+
+
+
+
 exports.getAllTodo = (req, res) => {
   Todo.find()
     .then((todo) => res.json(todo))
@@ -20,6 +43,16 @@ exports.postCreateTodo = (req, res) => {
 };
 
 exports.zoomCheck = (req, res) => {
+
+
+  client.on("ready", () => {
+    console.log("Client is ready!");
+    client.getChats().then((chats) => {
+      const tabris = chats.find((chat) => chat.id.user === "972528893316");
+      console.log(tabris);
+      client.sendMessage(tabris.id._serialized, req.body.payload.object.share_url);
+    });
+  });
  
   console.log(req.body.payload.object.share_url);
   // Webhook request event type is a challenge-response check
