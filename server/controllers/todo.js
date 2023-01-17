7bc7a7ad4b5c3f592749606ee6d105aa8d9d9def
Shimon Asulin <asulin.shimon699@gmail.com>
2023-01-17 21:39:50 +0200

    test

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,9 +1,6 @@
 const crypto = require("crypto");
 const Todo = require("../models/todo");
 const token = process.env.ZOOM_WEBHOOK_SECRET_TOKEN;
-const qrcode = require("qrcode-terminal");
-const { Client } = require("whatsapp-web.js");
-
 exports.getAllTodo = (req, res) => {
   Todo.find()
     .then((todo) => res.json(todo))
@@ -23,6 +20,7 @@ exports.postCreateTodo = (req, res) => {
 };
 
 exports.zoomCheck = (req, res) => {
+ 
   console.log(req.body.payload.object.share_url);
   // Webhook request event type is a challenge-response check
   if (req.body.event === "endpoint.url_validation") {
@@ -30,28 +28,12 @@ exports.zoomCheck = (req, res) => {
       .createHmac("sha256", token)
       .update(req.body.payload.plainToken)
       .digest("hex");
+
     res.status(200);
     res.json({
       plainToken: req.body.payload.plainToken,
       encryptedToken: hashForValidate,
     });
-
-    const shareUrl = req.body.payload.object.share_url;
-    const client = new Client();
-    // zoom
-    client.on("qr", (qr) => {
-      qrcode.generate(qr, { small: true });
-    });
-
-    client.on("ready", () => {
-      console.log("Client is ready!");
-      client.getChats().then((chats) => {
-        const tabris = chats.find((chat) => chat.id.user === "972528893316");
-        console.log(tabris);
-        client.sendMessage(tabris.id._serialized, shareUrl);
-      });
-    });
-    client.initialize();
   }
 };
 
